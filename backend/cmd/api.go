f26eee5ea11067d08c7bfcfc3a3f7bf6505f9745
André Simões <andre.simoesdutra23@gmail.com>
2025-12-11 00:07:43 -0300

    feat: add cors config for chi

--- backend/cmd/api.go
+++ backend/cmd/api.go
@@ -13,11 +13,22 @@ import (
 	"github.com/euandresimoes/visualdb-go.git/internal/modules/tables"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func (api *ApiConfig) Init() {
 	r := chi.NewRouter()
+
+	// Basic CORS
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 
 	// A good base middleware stack
 	r.Use(middleware.RequestID)
